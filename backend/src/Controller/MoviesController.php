97b1fdc73f28958ccf56d7f0cc8dd3e09c00f749
Riccardo Nestola <riccardinux@gmail.com>
2024-05-07 01:18:54 +0200

    add queryBuilder to MoviesController

--- backend/src/Controller/MoviesController.php
+++ backend/src/Controller/MoviesController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class MoviesController extends AbstractController
 {
@@ -16,11 +17,43 @@ class MoviesController extends AbstractController
     ) {}
 
     #[Route('/movies', methods: ['GET'])]
-    public function list(): JsonResponse
-    {
-        $movies = $this->movieRepository->findAll();
-        $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
+public function list(Request $request): JsonResponse
+{
+    $sortBy = $request->query->get('sort');
+    $genre = $request->query->get('genre');
+    $rating = $request->query->get('rating');
 
-        return new JsonResponse($data, json: true);
+    $queryBuilder = $this->movieRepository->createQueryBuilder('m')
+        ->leftJoin('m.movieGenres', 'mg')
+        ->leftJoin('mg.genre', 'g');  
+
+    if ($genre) {
+        $queryBuilder->andWhere('g.id = :genreId')  
+                     ->setParameter('genreId', $genre);
     }
+
+    if ($rating) {
+    $queryBuilder->andWhere('m.rating = :rating')
+                 ->setParameter('rating', $rating);
+}
+
+
+    if ($sortBy) {
+        switch ($sortBy) {
+            case 'recent':
+                $queryBuilder->orderBy('m.releaseDate', 'DESC');
+                break;
+            case 'rating':
+                $queryBuilder->orderBy('m.rating', 'DESC');
+                break;
+        }
+    }
+
+    $movies = $queryBuilder->getQuery()->getResult();
+    $data = $this->serializer->serialize($movies, 'json', ['groups' => 'default']);
+
+    return new JsonResponse($data, json: true);
+}
+
+    
 }
